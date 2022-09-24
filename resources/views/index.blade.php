f13aa2941bc06a349d6c8902e13a390e37860baa
Michelle Christina <michellechristina89@gmail.com>
2022-09-24 13:02:23 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+
+<<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title> CURRICULUM VITAE</title>
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/fontawsom-all.min.css">
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    </head>
+
+    <body>
+        <div class="container-fluid overcover">
+            <div class="container profile-box">
+                <div class="row">
+                    <div class="col-md-4 left-co">
+                        <div class="left-side">
+                            <div class="profile-info">
+                                <img src="/img/foto.jpeg" alt="" width="150" height="200">
+                                <h3>Michelle Christina</h3>
+                            </div>
+                            <h4 class="ltitle">Contact</h4>
+                            <div class="contact-box pb0">
+                                <div class="icon">
+                                    <i class="fas fa-phone"></i>
+                                </div>
+                                <div class="detail">
+                                    +62 89531630502 <br>
+                                </div>
+                            </div>
+                            <div class="contact-box pb0">
+                                <div class="icon">
+                                    <i class="fas fa-globe-americas"></i>
+                                </div>
+                                <div class="detail">
+                                    michellechristina89@gmail.com <br>
+                                </div>
+                            </div>
+                            <div class="contact-box">
+                                <div class="icon">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                </div>
+                                <div class="detail">
+                                     Bandung
+                                </div>
+                            </div>
+                            <h4 class="ltitle">Contact</h4>
+                            <ul class="row social-link no-margin">
+                                <li><i class="fab fa-facebook-f"></i></li>
+                                <li><i class="fab fa-twitter"></i></li>
+                                <li><i class="fab fa-google-plus-g"></i></li>
+                                <li><i class="fab fa-linkedin-in"></i></li>
+                                <li><i class="fab fa-github"></i></li>
+                            </ul>
+
+                            <h4 class="ltitle">Hobbies</h4>
+                            <ul class="hoby row no-margin">
+                                <li><i class="fas fa-pencil-alt"></i> <br> Writing</li>
+                                <li><i class="fas fa-film"></i><br> Movies</li>
+                                <li><i class="fas fa-plane-departure"></i> <br>Travel</li>
+                                <li><i class="fas fa-gamepad"></i> <br> Games</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-8 rt-div">
+                        <div class="rit-cover">
+                            <div class="hotkey">
+                                <h1 class="">Michelle Christina </h1>
+                                <small>2021110053</small>
+                            </div>
+                            <h2 class="rit-titl"><i class="far fa-user"></i> Profile</h2>
+                            <div class="about">
+                                <p>I am flexible, reliable and possess excellent time keeping skills. I am an enthusiastic, self-motivated, reliable, responsible and hard working person. I am a mature team worker and adaptable to all challenging situations. I am able to work well both in a team environment as well as using own initiative.</p>
+                                <div class="btn-ro row no-margin">
+                                </div>
+                            </div>
+
+                            <h2 class="rit-titl"><i class="fas fa-briefcase"></i> Work Experiance</h2>
+                            <div class="work-exp">
+                                <h6>Teller <span>2017-2019</span></h6>
+                                <i>Bank Central Asia</i>
+                                <ul>
+                                    <li><i class="far fa-hand-point-right"></i> Mencatat transaksi, yang melibatkan pencatatan cek dan menyiapkan laporan transaksi </li>
+                                    <li><i class="far fa-hand-point-right"></i> Menghitung dan mengemas mata uang </li>
+                                    <li><i class="far fa-hand-point-right"></i> Mempromosikan produk dan layanan bank </li>
+                                </ul>
+
+                            <h2 class="rit-titl"><i class="fas fa-graduation-cap"></i> Education</h2>
+                            <div class="education">
+                                <ul class="row no-margin">
+                                    <li class="col-md-6"><span>2003-2004</span> <br>
+                                        TKK BPK Penabur Bandung</li>
+                                    <li class="col-md-6"><span>2004-2010</span> <br>
+                                        SDK 6 BPK Penabur Bandung</li>
+                                    <li class="col-md-6"><span>2010-2013</span> <br>
+                                        SMP Waringin Bandung</li>
+                                    <li class="col-md-6"><span>2013-2017</span> <br>
+                                        SMA Trinitas Bandung</li>
+                                </ul>
+                            </div>
+
+                            <h2 class="rit-titl"><i class="fas fa-users-cog"></i> Skills</h2>
+                            <div class="profess-cover row no-margin">
+                                <div class="col-md-6">
+                                    <div class=" prog-row row">
+                                        <div class="col-sm-6">
+                                            Photoshop
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="progress">
+                                                <div class="progress-bar" role="progressbar" style="width: 65%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="row prog-row">
+                                        <div class="col-sm-6">
+                                            Microsoft
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="progress">
+                                                <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="row prog-row">
+                                        <div class="col-sm-6">
+                                            Web Design
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="progress">
+                                                <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="row prog-row">
+                                        <div class="col-sm-6">
+                                            Java
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="progress">
+                                                <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/script.js"></script>
+
+
+    </html>
+
+{{-- <html>
+
+<head>
+    <title>CURRICULUM VITAE</title>
+    <link rel="stylesheet" href="/css/app.css" />
+</head>
+
+<body>
+
+    <h1> CURRICULUM VITAE </h1>
+    <hr>
+        <b>PERSONAL INFORMATION</b>
+        <hr>
+        <table>
+            <img src="/img/foto.jpeg" align="right" width="500" height="500">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Nama Lengkap</td>
+            <td>: Michelle Christina </td>
+        </tr>
+        <tr>
+            <td>Tempat, Tanggal Lahir</td>
+            <td>: Bandung, 25 September 1999 </td>
+        </tr>
+            <td>Alamat</td>
+            <td>: Jalan Aksan No 2/84</td>
+        <tr>
+            <td>Status</td>
+            <td>: Mahasiswa</td>
+        </tr>
+            <tr>
+            <td>No HP</td>
+            <td>: 089531630502</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: michellechristina89@gmail.com</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>: Perempuan</td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>: Kristen</td>
+        </tr>
+        </table>
+        <hr>
+        <b>EDUCATION </b>
+        <hr>
+        <table>
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>2014-2017</td>
+            <td>SMA Trinitas Bandung </td>
+        </tr>
+        <tr>
+            <td>2010-2013</td>
+            <td>SMP Waringin Bandung </td>
+        </tr>
+            <td>2004-2010</td>
+            <td>SDK 6 BPK Penabur Bandung</td>
+        <tr>
+            <td>2003-2004</td>
+            <td>TKK BPK Penabur Bandung</td>
+        </tr>
+        </table>
+        <hr>
+        <b>Expertise</b><hr>
+        Mengoperasikan Komputer : Microsoft Office<br>
+        Desain Grafis: CorelDRAW & Photoshop<br>
+
+
+    <script src="/js/app.js"></script>
+
+ </body>
+
+</html> --}}
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,356 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.101.0">
+    <title>Galeri</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/album/">
+
+
+
+    <!-- Bootstrap core CSS -->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="/css/album.css" rel="stylesheet">
+  </head>
+  <body>
+
+<header>
+  <div class="collapse bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <ul class="list-unstyled">
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container d-flex justify-content-between">
+      <a href="#" class="navbar-brand d-flex align-items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
+        <strong>Captures</strong>
+
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
+
+<main role="main">
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1>Photography</h1>
+      <p>Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film.</p>
+      <p>Photographic memory is something that people claim they have when they can recall an image or scene exactly as it was.</p>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_01.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">When our memories outweigh our dreams, we have grown old. -Bill Clinton</p>
+                <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_02.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Remembering is easy. It's forgetting that's hard. -Brodi Ashton</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_03.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Be careful who you make memories with. Those things can last forever. -Ugo Eze</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_04.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Take care of all your memories, for you cannot relive them. -Bob Dylan</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_05.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Memories are like antiques, the older they are, the more valuable they became. -Marinela Reka</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_06.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Memories are what warm you up from the inside. But they're also what tear you apart. -Haruki Murakami</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_08.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Take up one idea. Make that one idea your life--think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success. -Swami Vivekananda</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_09.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Success is walking from failure to failure with no loss of enthusiasm. -Winston Churchill</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_10.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">He who is not courageous enough to take risks will accomplish nothing in life. -Muhammad Ali</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_11.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">You can have anything you want if you want it badly enough. You can be anything you want to be, do anything you set out to accomplish if you hold to that desire with singleness of purpose. -Abraham Lincoln</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/gallery_12.jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Work hard for what you want because it won't come to you without a fight. You have to be strong and courageous and know that you can do anything you put your mind to. If somebody puts you down or criticizes you, just keep on believing in yourself and turn it into something positive. -Leah LaBelle</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_13.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">Lost time is never found again. -Benjamin Franklin</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_14.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">The best revenge is massive success. -Frank Sinatra</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_15.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">Better three hours too soon than a minute too late. -William Shakespeare</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_16.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">It always seems impossible until it's done. -Nelson Mandela</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_17.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">Don't cry because it's over, smile because it happened. -Dr. Seuss</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_18.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">Education is not learning of facts, but the training of the mind to think. -Albert Einstein</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_19.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">Life is what happens when you're busy making other plans. -John Lennon</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_20.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">We cannot solve our problems with the same thinking we used when we created them. -Albert Einstein</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <img src="/img/gallery_21.jpg" alt="">
+              <div class="card-body">
+                  <p class="card-text">The best way to find yourself is to lose yourself in the service of others. -Mahatma Gandhi</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src="/img/download (1).jpg" alt="">
+            <div class="card-body">
+                <p class="card-text">Great things are done by a series of small things brought together. -Vincent Van Gogh</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<footer class="text-muted">
+  <div class="container">
+    <p class="float-right">
+  </div>
+</footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+  </body>
+</html>
